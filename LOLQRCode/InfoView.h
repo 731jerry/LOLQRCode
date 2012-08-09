18d99bc99171c9b65fb8b4c9f43a5375d2d16a44
JerryZhu <731jerry@gmail.com>
2012-08-08 22:09:40 -0400

    添加了icon info页面(info.html)

---
+++ LOLQRCode/InfoView.h
@@ -0,0 +1,14 @@
+//
+//  InfoView.h
+//  LOLQRCode
+//
+//  Created by Jerry Zhu on 8/8/12.
+//  Copyright (c) 2012 Jerry Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoView : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *infoWebView;
+
+@end
---
+++ LOLQRCode/InfoView.m
@@ -0,0 +1,51 @@
+//
+//  InfoView.m
+//  LOLQRCode
+//
+//  Created by Jerry Zhu on 8/8/12.
+//  Copyright (c) 2012 Jerry Zhu. All rights reserved.
+//
+
+#import "InfoView.h"
+
+@interface InfoView ()
+
+@end
+
+@implementation InfoView
+@synthesize infoWebView = _infoWebView;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"info"] ofType:@"html"];
+    NSFileHandle *readHandle = [NSFileHandle fileHandleForReadingAtPath:path];
+    NSString *htmlString = [[NSString alloc] initWithData:
+                            [readHandle readDataToEndOfFile] encoding:NSUTF8StringEncoding];
+    [self.infoWebView loadHTMLString:htmlString baseURL:nil];
+}
+
+- (void)viewDidUnload
+{
+    [self setInfoWebView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
