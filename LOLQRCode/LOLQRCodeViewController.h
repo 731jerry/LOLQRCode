67b7fab96887489349ceed1cea464a8659f63334
JerryZhu <731jerry@gmail.com>
2012-08-09 11:01:21 -0400

    修改button 的设计 把button变成glossy 就是抛光

--- LOLQRCode/LOLQRCodeViewController.h
+++ LOLQRCode/LOLQRCodeViewController.h
@@ -8,12 +8,21 @@
 
 #import <UIKit/UIKit.h>
 #import "ZbarSDK.h"
+#import "GradientButton.h"
 
-
-@interface LOLQRCodeViewController : UIViewController < ZBarReaderDelegate,UIAlertViewDelegate >
+@interface LOLQRCodeViewController : UIViewController < ZBarReaderDelegate,UIAlertViewDelegate >{
+    GradientButton *generateQRCodeButton;
+    GradientButton *saveImageButton;
+    GradientButton *scanQRCodeButton;
+    
+}
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UITextField *inputText;
 @property (weak, nonatomic) IBOutlet UILabel *warnningLabel;
+
+@property (nonatomic, retain) IBOutlet GradientButton *generateQRCodeButton;
+@property (nonatomic, retain) IBOutlet GradientButton *saveImageButton;
+@property (nonatomic, retain) IBOutlet GradientButton *scanQRCodeButton;
 
 - (IBAction)generateQRCode:(id)sender;
 - (IBAction)saveImage:(id)sender;
